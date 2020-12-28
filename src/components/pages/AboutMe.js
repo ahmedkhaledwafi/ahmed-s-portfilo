4daecd44844765316c98951025d1674b446324af
Ahmed Khaled <ahmed.khaled.hussain@gmail.com>
2020-12-28 10:05:33 +0200

    fix: Make about me page responsive

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -51,6 +51,7 @@ function AboutMe() {
                     </p>
 
                 <div className="about-me--contact-me">
+                    <h2 className="about-me--contact-me-head end sub-title">Contact Me</h2>
                     <p>Sphinx St - Mashal</p>
                     <br />
                     <p>Giza/Egypt</p>
@@ -80,7 +81,7 @@ function AboutMe() {
                             <li>Proficient understanding of client-side scripting and JavaScript frameworks, including jQuery and React.</li>
                         </ul>
                     </div>
-                    <div className="Skills-imgs">
+                    <div className="skills-imgs">
 
                         {/* Html */}
                         <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="html5" class="skills-img svg-inline--fa fa-html5 fa-w-12" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="#FD7E14" d="M0 32l34.9 395.8L191.5 480l157.6-52.2L384 32H0zm308.2 127.9H124.4l4.1 49.4h175.6l-13.6 148.4-97.9 27v.3h-1.1l-98.7-27.3-6-75.8h47.7L138 320l53.5 14.5 53.7-14.5 6-62.2H84.3L71.5 112.2h241.1l-4.4 47.7z"></path></svg>
