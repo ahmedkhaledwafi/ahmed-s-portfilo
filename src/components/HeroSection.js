d581a2de04aa1092b72cc2f8ec52cc05f2ee1992
Ahmed Khaled <ahmed.khaled.hussain@gmail.com>
2020-12-28 08:50:29 +0200

    style: remove Watch Trailer button

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -17,13 +17,13 @@ function HeroSection() {
                 </Button>
             </div>
             <div className="hero-btns">
-                <Button className='btns'
+                {/* <Button className='btns'
                     buttonStyle="btn--primary"
                     buttonSize='btn--large'
                     buttonLink='/Resume-Ahmed-Khaled.pdf'
                 >
                     WATCH TRAILER <i className="far fa-play-circle" />
-                </Button>
+                </Button> */}
             </div>
         </div>
     )
