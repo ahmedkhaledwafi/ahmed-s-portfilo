9d4aa3bcc916d78310f66bc206431029ba24a6d7
Ahmed Khaled <ahmed.khaled.hussain@gmail.com>
2021-01-05 09:04:11 +0200

    fix: Hero header below the navbar

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -3,29 +3,32 @@ import { Button } from './Button';
 import './HeroSection.css';
 function HeroSection() {
     return (
-        <div className="hero-container">
-            {/* <video src="/videos/video-1.mp4" autoPlay loop muted /> */}
-            <h1 className="hero-container--header">Want to Meet Awesome Frontend Developer</h1>
-            <p>Meet Ahmed Khaled</p>
-            <div className="hero-btns">
-                <Button className='btns'
-                    buttonStyle="btn--outline"
-                    buttonSize='btn--large'
-                    buttonLink='/Resume-Ahmed-Khaled.pdf'
-                >
-                    Download Resume
+        <>
+            <div className="hero-padding" />
+            <div className="hero-container">
+                {/* <video src="/videos/video-1.mp4" autoPlay loop muted /> */}
+                <h1 className="hero-container--header">Want to Meet Awesome Frontend Developer</h1>
+                <p>Meet Ahmed Khaled</p>
+                <div className="hero-btns">
+                    <Button className='btns'
+                        buttonStyle="btn--outline"
+                        buttonSize='btn--large'
+                        buttonLink='/Resume-Ahmed-Khaled.pdf'
+                    >
+                        Download Resume
                 </Button>
-            </div>
-            <div className="hero-btns">
-                {/* <Button className='btns'
+                </div>
+                <div className="hero-btns">
+                    {/* <Button className='btns'
                     buttonStyle="btn--primary"
                     buttonSize='btn--large'
                     buttonLink='/Resume-Ahmed-Khaled.pdf'
                 >
                     WATCH TRAILER <i className="far fa-play-circle" />
                 </Button> */}
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
