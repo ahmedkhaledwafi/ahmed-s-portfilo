12a4301766f0f83d0252f08bc09a8d1cdc19c18c
Ahmed Khaled <ahmed.khaled.hussain@gmail.com>
2021-01-07 08:38:30 +0200

    style: Add padding to contact page

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { Button } from '../Button/Button';
 import './HeroSection.css';
 import { TweenLite, Power3 } from 'gsap'
