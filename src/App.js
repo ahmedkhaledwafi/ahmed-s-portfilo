7dc74913479bc0804455e5a2d3218f635cbe3882
Ahmed Khaled <ahmed.khaled.hussain@gmail.com>
2021-01-31 08:21:09 +0200

    feat: add spinner while loading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
@@ -8,23 +8,48 @@ import ContactMe from "./components/pages/ContactMe";
 import Footer from "./components/Footer/Footer";
 import { CSSPlugin } from 'gsap/CSSPlugin';
 import NotFound from "./components/pages/NotFound";
+import ClipLoader from "react-spinners/ClipLoader";
 
 const C = CSSPlugin; // eslint-disable-line
 
 function App() {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        // setLoading(false)
+        setTimeout(() => {
+            setLoading(false)
+        }, 1000)
+
+    }, [])
     return (
-        <>
-            <Router>
-                <Navbar />
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route exact path='/about-me' component={AboutMe} />
-                    <Route exact path='/contact-me' component={ContactMe} />
-                    <Route path='/*' component={NotFound} />
-                </Switch>
-                <Footer />
-            </Router>
-        </>
+        <div className="App">
+            {
+                loading
+
+                    ?
+                    <div className="loader">
+                        <ClipLoader
+                            color="#9F1555"
+                            loading={"loading"}
+                            size="15vw"
+                        />
+                    </div>
+
+                    :
+
+                    <Router>
+                        <Navbar />
+                        <Switch>
+                            <Route exact path="/" component={Home} />
+                            <Route exact path='/about-me' component={AboutMe} />
+                            <Route exact path='/contact-me' component={ContactMe} />
+                            <Route path='/*' component={NotFound} />
+                        </Switch>
+                        <Footer />
+                    </Router>
+            }
+        </div>
     );
 }
 
